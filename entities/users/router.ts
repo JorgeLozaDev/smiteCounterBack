4ab0c527f6fae01817649fe922b0e423507d180b
jorge <j0rg3e93@gmail.com>
2023-12-26 09:46:56 +0100

    UPDATE: se agrega en el router la llamada get de getProfile

--- entities/users/router.ts
+++ entities/users/router.ts
@@ -1,10 +1,11 @@
 import express, { Request, Response } from "express";
 import { authMiddleware } from "../../middlewares/authMiddleware";
-import { loginUser, singIn } from "./controller";
+import { getProfile, loginUser, singIn } from "./controller";
 
 const router = express.Router();
 
 router.post("/addUser", singIn); 
 router.post("/login", loginUser);
+router.get("/profile",authMiddleware, getProfile);
 
 export default router;
