4df592876b828a44dfb8f4712cbc9b39e75d9737
jorge <j0rg3e93@gmail.com>
2023-12-26 09:45:07 +0100

    UPDATE: se agregan en el controlador de user, la función de getProfile, para obtener todos los datos del usuario logueado

--- entities/users/controller.ts
+++ entities/users/controller.ts
@@ -59,7 +59,6 @@ export const singIn = async (
   }
 };
 
-
 export const loginUser = async (
   req: Request,
   res: Response,
@@ -95,6 +94,30 @@ export const loginUser = async (
     );
 
     res.status(200).json({ token });
+  } catch (error) {
+    next(error);
+  }
+};
+
+// Controlador para obtener el perfil del usuario
+export const getProfile = async (
+  req: AuthenticatedRequest,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    const userId = req.user.id; // Obtén el ID del usuario autenticado desde el middleware
+
+    // Busca al usuario por ID
+    const user = await User.findById(userId);
+
+    if (!user) {
+      const error = new Error("Usuario no encontrado");
+      (error as any).status = 404;
+      throw error;
+    }
+
+    res.status(200).json(user);
   } catch (error) {
     next(error);
   }
